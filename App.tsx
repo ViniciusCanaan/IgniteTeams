969f2d214a1744d629aef87d622e8c8e48e55766
Vinicius Canaan <60820733+ViniciusCanaan@users.noreply.github.com>
2023-03-14 18:03:50 -0300

    Ajustando Statusbar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
+import { StatusBar } from 'react-native';
 import { Groups } from '@screens/Groups';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { ActivityIndicator } from 'react-native';
+
+import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
 
@@ -12,7 +14,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }

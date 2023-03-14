6a9945a048e2b038a9c4d7569b349c450128ca47
Vinicius Canaan <60820733+ViniciusCanaan@users.noreply.github.com>
2023-03-14 16:17:07 -0300

    Inserindo cores, fontes, e ThemProvider

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
 import { Groups } from '@screens/Groups';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
+
+import theme from './src/theme';
+
+import { ThemeProvider } from 'styled-components';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }

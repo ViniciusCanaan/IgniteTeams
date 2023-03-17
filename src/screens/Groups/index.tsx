bde98c078b2c1cdc4d1b300254de7f4efb983d89
Vinicius Canaan <60820733+ViniciusCanaan@users.noreply.github.com>
2023-03-17 18:19:06 -0300

    Criando componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@components/Header";
 import { HighLight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 import React from "react";
 
 import { Container } from "./styles";
@@ -9,6 +10,7 @@ export function Groups() {
       <Container>
         <Header showBackButton/>
         <HighLight title="Turmas" subtitle="jogue com a sua turma"/>
+        <GroupCard title="Galera do Ignite"/>
       </Container>
     );
   }
